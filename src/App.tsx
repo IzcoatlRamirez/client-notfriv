5877baf32d7cb2023b048e74d7060417b912469c
Carlos Martin Garcia Madrigal <75263278+Carlos-Garcia09@users.noreply.github.com>
2023-11-27 21:36:18 -0600

    Ruta de ayuda

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import MainPage from "./pages/MainPage";
+import HelpPage from "./pages/HelpPage";
 
 function App() {
   return (
@@ -7,6 +8,7 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<MainPage />}></Route>
+          <Route path="/help" element={<HelpPage/>}/>
         </Routes>
       </BrowserRouter>
     </>
