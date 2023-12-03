b69dc4973cee89096b915f2b25b52c6cd807f11e
AmandaNomad <142647836+AmandaNomad@users.noreply.github.com>
2023-12-02 18:13:00 -0600

    estilos para el about
    se agregaron estilos para el about

--- src/components/CollaboratorCard.tsx
+++ src/components/CollaboratorCard.tsx
@@ -4,42 +4,9 @@ import {Typography } from "@mui/material";
   function CollaboratorCard(){
    return(
     <div>
-      <Typography variant="h4" sx={{ color: "white" }}>
-      Project Manager<br/><br/>
-      </Typography>
+    
       <Typography variant="h5" sx={{ color: "white" }}>
-      Angel Ignacio Ortega Vega <a  style={{color: "white",textDecoration: "none",}} href="https://angelortega03.github.io/">Consultar CV<br/></a>
-      </Typography><br/><br/>
-      <Typography variant="h4" sx={{ color: "white" }}>
-        EQUIPO FRONTEND<br/><br/>
-      </Typography>
-    <Typography variant="h5" sx={{ color: "white" }}>
-    Izcoatl Ramirez Cortes <a style={{color: "white",textDecoration: "none",}} href="https://izcoatlramirez.github.io/react-curriculum-app/">Consultar CV<br/></a>
-    Amanda De la Cruz Martinez <a style={{color: "white",textDecoration: "none",}} href="https://amandanomad.github.io/personalCv/">Consultar CV <br/></a>
-    Carlos Martin Garcia Madrigal <a style={{color: "white",textDecoration: "none",}}href="https://carlos-garcia09.github.io/">Consultar CV <br/></a>
-    Dafne J Crespo Romo <a style={{color: "white",textDecoration: "none",}}href="https://cvdaf.daffcrespo.repl.co/?authuser=0&hl=es">Consultar CV  <br/></a>
-    David Felipe Pinto Jimenez <a style={{color: "white",textDecoration: "none",}}href="https://davidpinto02.github.io/curriculum/">Consultar CV <br/></a>
-    Diego Israel Luna Curiel <a style={{color: "white",textDecoration: "none",}}href="https://diegolunacuriel.github.io/Maquetado-HTML-CSS/">Consultar CV <br/></a>
-    Eloisa Isabela Magos Durán <a style={{color: "white",textDecoration: "none",}}href="https://isabela-magos-maquetado-html-css.netlify.app/">Consultar CV <br/></a>
-    Gemma Isela Castañeda Hernández <a style={{color: "white",textDecoration: "none",}}href="https://gemmacastaneda9048.github.io/master/">Consultar CV <br/></a>
-    Kenya Sánchez Mercado <a style={{color: "white",textDecoration: "none",}}href="https://kenyamercado.github.io/t01-maquetado-html-css/">Consultar CV <br/></a>
-    Kevin Alejandro Aldrete Martinez <a style={{color: "white",textDecoration: "none",}}href="https://astounding-pudding-320da8.netlify.app">Consultar CV <br/></a>
-    Luis Armando Luna Hernández <a style={{color: "white",textDecoration: "none",}}href="https://battlesimphony13.github.io/Curriculum-Vitae-LUNA-HERNANDEZ-LUIS-ARMANDO/">Consultar CV <br/></a>
-    Oswaldo Javier España Trujillo <a style={{color: "white",textDecoration: "none",}}href="https://cv-oswaldo-espana.onrender.com/">Consultar CV <br/></a>
-    Samuel Israel Medina Rodríguez <a style={{color: "white",textDecoration: "none",}}href="https://samuelimr.github.io/my_CV/">Consultar CV <br/></a><br/><br/>
-    </Typography>
-      <Typography variant="h4" sx={{ color: "white" }}>
-        EQUIPO BACKEND<br/><br/>
-      </Typography>
-      <Typography variant="h5" sx={{ color: "white" }}>
-      Anthony Esteven Sandoval Marquez <a style={{color: "white",textDecoration: "none",}}href="https://zaikron.github.io/PROGINT_TRAB1_CV/">Consultar CV <br/></a>
-      Braulio Fermin Montes Rosales <a style={{color: "white",textDecoration: "none",}}href="https://mbraulio.github.io/cv/">Consultar CV <br/></a>
-      Diego Ivan Becerra Gonzalez <a style={{color: "white",textDecoration: "none",}}href="https://github.com/diegoivan1987/ProgInt/blob/tarea1/cv.pdf">Consultar CV <br/></a>
-      Emilio Josafat Giacomo Quintero <a style={{color: "white",textDecoration: "none",}}href="https://adoptsomekids.github.io/Maquetado-en-Html-Css-CV/">Consultar CV <br/></a>
-      Jahir Alejandro Rivera Avalos <a style={{color: "white",textDecoration: "none",}}href="https://imjahirrivera.github.io/">Consultar CV <br/></a>
-      Manuel Navarro Valadez <a style={{color: "white",textDecoration: "none",}}href="https://molki3.github.io/PPI-RESUME/">Consultar CV <br/></a>
-      Saul Alejandro Castañeda Pérez <a style={{color: "white",textDecoration: "none",}}href=" https://enpanada.github.io/CV_Template/">Consultar CV  <br/></a>
-      Uziel Cornejo Olivares <a style={{color: "white",textDecoration: "none",}}href="https://rr-uzi.github.io">Consultar CV <br/></a>
+    
       </Typography><br/><br/><br/><br/><br/><br/>
     </div>
    );
--- src/pages/AboutPage.tsx
+++ src/pages/AboutPage.tsx
@@ -1,46 +1,135 @@
 import TopBar from "../components/TopBar";
 import CollaboratorCard from "../components/CollaboratorCard";
-import { Box,Typography } from "@mui/material";
+import { Box, Typography } from "@mui/material";
+import React from "react";
 
-function AboutPage(){
-    return(
-        <>
-        <Box
+interface Collaborator {
+  name: string;
+  cvLink: string;
+}
+
+const ListItem: React.FC<Collaborator> = ({ name, cvLink }) => (
+  <li style={{ marginBottom: '15px', fontSize: '24px', display: 'flex', alignItems: 'center' }}>
+    <span style={{ fontWeight: 'bold', color: '#05166C', fontSize: '32px', verticalAlign: 'middle', marginRight: '10px' }}>•</span>
+    <div style={{ display: 'flex', alignItems: 'center', flex: '1', justifyContent: 'space-between', paddingRight: '1000px' }}>
+      <span style={{ color: 'white', fontFamily: 'optima' }}>{name}</span>
+      <a
+        style={{
+          color: 'white',
+          textDecoration: 'none',
+          padding: '5px 10px',
+          borderRadius: '5px',
+          backgroundColor: '#05166C',
+          fontFamily: 'geneva',
+          minWidth: '150px',
+          textAlign: 'center',
+        }}
+        href={cvLink}
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Consultar CV
+      </a>
+    </div>
+  </li>
+);
+
+function AboutPage() {
+
+  const collaboratorsF = [
+    { name: 'Izcoatl Ramirez Cortes', cvLink: 'https://izcoatlramirez.github.io/react-curriculum-app/' },
+    { name: 'Amanda De la Cruz Martinez', cvLink: 'https://amandanomad.github.io/personalCv/' },
+    { name: 'Carlos Martin Garcia Madrigal', cvLink: 'https://carlos-garcia09.github.io/' },
+    { name: 'Dafne J Crespo Romo', cvLink: 'https://cvdaf.daffcrespo.repl.co/?authuser=0&hl=es' },
+    { name: 'David Felipe Pinto Jimenez', cvLink: 'https://davidpinto02.github.io/curriculum/' },
+    { name: 'Diego Israel Luna Curiel', cvLink: 'https://diegolunacuriel.github.io/Maquetado-HTML-CSS/' },
+    { name: 'Eloisa Isabela Magos Durán', cvLink: 'https://isabela-magos-maquetado-html-css.netlify.app/' },
+    { name: 'Gemma Isela Castañeda Hernández', cvLink: 'https://gemmacastaneda9048.github.io/master/' },
+    { name: 'Kenya Sánchez Mercado', cvLink: 'https://kenyamercado.github.io/t01-maquetado-html-css/' },
+    { name: 'Kevin Alejandro Aldrete Martinez', cvLink: 'https://astounding-pudding-320da8.netlify.app' },
+    { name: 'Luis Armando Luna Hernández', cvLink: 'https://battlesimphony13.github.io/Curriculum-Vitae-LUNA-HERNANDEZ-LUIS-ARMANDO/' },
+    { name: 'Oswaldo Javier España Trujillo ', cvLink: 'https://cv-oswaldo-espana.onrender.com/' },
+    { name: 'Samuel Israel Medina Rodríguez', cvLink: 'https://samuelimr.github.io/my_CV/' },
+
+
+  ];
+
+  const collaboratorsB = [
+
+    { name: 'Anthony Esteven Sandoval Marquez', cvLink: 'https://zaikron.github.io/PROGINT_TRAB1_CV/' },
+    { name: 'Braulio Fermin Montes Rosales', cvLink: 'https://mbraulio.github.io/cv/' },
+    { name: 'Diego Ivan Becerra Gonzalez', cvLink: 'https://github.com/diegoivan1987/ProgInt/blob/tarea1/cv.pdf' },
+    { name: 'Emilio Josafat Giacomo Quintero ', cvLink: 'https://adoptsomekids.github.io/Maquetado-en-Html-Css-CV/' },
+    { name: 'Jahir Alejandro Rivera Avalos', cvLink: 'https://imjahirrivera.github.io/' },
+    { name: 'Manuel Navarro Valadez', cvLink: 'https://molki3.github.io/PPI-RESUME/' },
+    { name: 'Saul Alejandro Castañeda Pérez', cvLink: 'https://enpanada.github.io/CV_Template/' },
+    { name: 'Uziel Cornejo Olivares', cvLink: 'https://rr-uzi.github.io' }
+  ]
+
+  return (
+    <>
+      <Box
         sx={{
           display: "flex",
           alignItems: "center",
           justifyContent: "center",
           backgroundColor: "#1B1B1C",
         }}
-        >
+      >
         <TopBar />
-        </Box>
+      </Box>
       <Box
-            sx={{
-              display: "flex",
-              flexDirection: "column",
-              alignItems: "center",
-              mt: 2,
-              px: 4,
-            }}
-          >
-            <Box
-              sx={{
-                maxWidth: "60%",
-                textAlign: "center",
-                mb: 2,
-                marginTop: "20px",
-              }}
-            >
-              <Typography variant="h3" style={{ color: "white" }}>
-                Colaboradores
-              </Typography>
-            </Box>
-    
-            <CollaboratorCard/>
-            </Box>
-      </>
-    );
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          mt: 2,
+          px: 4,
+        }}
+      >
+        <Box
+          sx={{
+            maxWidth: "60%",
+            textAlign: "center",
+            mb: 2,
+            marginTop: "20px",
+          }}
+        >
+          <Typography variant="h1" style={{ color: "#05166C", fontFamily: "monaco" }}>
+            Colaboradores
+          </Typography>
+        </Box>
+
+        <Typography variant="h4" sx={{ color: "#070301", fontWeight: 'bold', fontSize: 30, fontFamily: "Times" }}>
+          PROJECT MANAGER<br /><br />
+        </Typography>
+        <Typography variant="h5" sx={{ color: "white", fontFamily: "optima", marginLeft: '10px'  }}>
+        <span style={{ color: '#05166C', fontWeight: 'bold', fontSize: '32px', verticalAlign: 'middle', marginRight: '10px' }}>•</span>Angel Ignacio Ortega Vega <a style={{ color: "white", textDecoration: 'none', marginLeft: '10px', padding: '5px 10px', borderRadius: '5px', backgroundColor: '#05166C', fontFamily: "geneva"}} href="https://angelortega03.github.io/">Consultar CV<br /></a>
+        </Typography><br /><br />
+        <Typography variant="h4" sx={{ color: "#070301", fontWeight: 'bold', fontSize: 30, fontFamily: "Times" }}>
+          EQUIPO FRONTEND<br /><br /><br/>
+        </Typography>
+        <ul style={{ listStyleType: 'none', padding: 0, width: '100%', textAlign: 'center' }}>
+          {collaboratorsF.map((collaborator, index) => (
+            <ListItem key={index} name={collaborator.name} cvLink={collaborator.cvLink} />
+          ))}
+        </ul>
+
+        <br/><br/><br/>
+        <Typography variant="h4" sx={{ color: "#070301", fontWeight: 'bold', fontSize: 30, fontFamily: "Times" }}>
+          EQUIPO BACKEND<br /><br /><br/>
+        </Typography>
+
+        <ul style={{ listStyleType: 'none', padding: 0, width: '100%', textAlign: 'center' }}>
+          {collaboratorsB.map((collaborator, index) => (
+            <ListItem key={index} name={collaborator.name} cvLink={collaborator.cvLink} />
+          ))}
+        </ul>
+
+        <CollaboratorCard />
+      </Box>
+    </>
+  );
 }
 
 export default AboutPage;
